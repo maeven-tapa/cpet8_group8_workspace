0c1e05a55ddf03c186f67420a22fbdad89f9bf09
maeven-tapa <maeven.tapa@gsfe.tupcavite.edu.ph>
2025-04-03 05:14:06 +0800

    adding function the python file

--- EALS UI/v0.3/main(admin_only).py
+++ EALS UI/v0.3/main(admin_only).py
@@ -1,7 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QWidget, QStackedWidget, QTabWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QWidget, QStackedWidget, QTabWidget, QMessageBox
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import Qt
+from datetime import datetime
 
 class EALS:
     def __init__(self):
@@ -11,26 +12,86 @@ class EALS:
         global_home_ui.showMaximized()
 
 class Home:
+    admin_password = "defaultpassword"
+    password_changed = False
+    
     def __init__(self):
         self.loader = QUiLoader()
         self.home_ui = self.loader.load("home.ui")
         self.home_ui.main_page.setCurrentWidget(self.home_ui.home_page)
+        self.admin_id = "admin-01-0001"
+        self.update_date_today()
+        self.home_ui.home_login_btn.clicked.connect(self.handle_login)
+    
+    def update_date_today(self):
+        current_date = datetime.now()
+        formatted_date = current_date.strftime("%a, %b %d, %Y")
+        self.home_ui.date_today.setText(formatted_date)
 
-        self.home_ui.home_login_btn.clicked.connect(self.goto_change_pass)
+    def handle_login(self):
+        user_id = self.home_ui.home_id_box.text()
+        password = self.home_ui.home_pass_box.text()
+        if user_id == self.admin_id and password == Home.admin_password:
+            if Home.password_changed:
+                self.goto_admin_ui()
+            else:
+                self.goto_change_pass()
+        else:
+            error_msg = QMessageBox()
+            error_msg.setIcon(QMessageBox.Critical)
+            error_msg.setText("Invalid credentials")
+            error_msg.setInformativeText("Please enter valid admin ID and password")
+            error_msg.setWindowTitle("Login Error")
+            error_msg.exec()
     
     def goto_change_pass(self):
         self.changepass = ChangePassword()
         self.changepass.change_pass_ui.show()
-
+    
+    def goto_admin_ui(self):
+        global_home_ui.close()
+        self.admin = Admin()
+        self.admin.admin_ui.showMaximized()
 
 class ChangePassword:
     def __init__(self):
         self.loader = QUiLoader()
         self.change_pass_ui = self.loader.load("admin_change_pass.ui")
-        self.change_pass_ui.admin_change_pass_btn.clicked.connect(self.goto_admin_home)
+        self.change_pass_ui.change_pass_note.setText("Please enter your current password and set a new one.")
+        self.change_pass_ui.change_pass_note.setStyleSheet("color: black")
+        self.change_pass_ui.admin_change_pass_btn.clicked.connect(self.validate_and_change_password)
         self.change_pass_ui.setWindowFlags(self.change_pass_ui.windowFlags() | Qt.WindowStaysOnTopHint)
 
+    def validate_and_change_password(self):
+        new_password = self.change_pass_ui.change_pass_np_box.text()
+        confirm_password = self.change_pass_ui.change_pass_confirm_box.text()
+        current_password = self.change_pass_ui.change_pass_cp_box.text()
+        
+        if current_password != Home.admin_password:
+            self.change_pass_ui.change_pass_note.setText("Current password is incorrect!")
+            self.change_pass_ui.change_pass_note.setStyleSheet("color: red")
+            return
+            
+        if not current_password or not new_password or not confirm_password:
+            self.change_pass_ui.change_pass_note.setText("All password fields are required!")
+            self.change_pass_ui.change_pass_note.setStyleSheet("color: red")
+            return
+            
+        if new_password != confirm_password:
+            self.change_pass_ui.change_pass_note.setText("New password and confirmation don't match!")
+            self.change_pass_ui.change_pass_note.setStyleSheet("color: red")
+            return
+            
+        if len(new_password) < 8:
+            self.change_pass_ui.change_pass_note.setText("New password must be at least 8 characters!")
+            self.change_pass_ui.change_pass_note.setStyleSheet("color: red")
+            return
+            
 
+        Home.admin_password = new_password
+        Home.password_changed = True
+        self.goto_admin_home()
+        
     def goto_admin_home(self):
         global_home_ui.close()
         self.change_pass_ui.close()
@@ -74,7 +135,13 @@ class Admin:
         self.admin_ui.hr_view_btn.clicked.connect(self.goto_hr_view)
         self.admin_ui.hr_view_back.clicked.connect(self.goto_employee_hr)
         
+        self.update_date_today()
 
+
+    def update_date_today(self):
+        current_date = datetime.now()
+        formatted_date = current_date.strftime("%a, %b %d, %Y")
+        self.admin_ui.date_today.setText(formatted_date)
 
     def goto_home(self):
         global_home_ui.showMaximized()
